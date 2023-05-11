e01bb1651498abde8232cd50d32f9be31f4b0b80
mabarbar <mateusz.barzenc00@gmail.com>
2023-05-11 14:47:45 +0200

    wiki rocket finished

---
+++ next4/app/[searchTerm]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+    <main className="bg-slate-200 mx-auto max-w-lg p-1 min-h-screen">
+      <h2 className="m-4 text-2xl font-bold">Loading...</h2>
+    </main>
+  );
+}
