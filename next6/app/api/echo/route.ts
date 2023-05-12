c6b32ef05c2e46eb6d0614ada9e3c00cc5bc7bc6
mabarbar <mateusz.barzenc00@gmail.com>
2023-05-12 15:59:18 +0200

    trying out backend api

---
+++ next6/app/api/echo/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  // const name = searchParams.get("name");
+  // const instrument = searchParams.get("instrument");
+
+  // return NextResponse.json({ name, instrument });
+  
+  // podejscie poniezej pozwala na dowolne parametry
+  const obj = Object.fromEntries(searchParams.entries());
+  
+  return NextResponse.json({ obj });
+}
